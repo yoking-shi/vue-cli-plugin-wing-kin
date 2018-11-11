691036b4a6f03883fa8327151dfc42055f8d8337
施永坚 <syj_188888888@163.com>
2018-11-11 18:03:31 +0800

    test

--- prompts.js
+++ prompts.js
@@ -1,15 +1,9 @@
 module.exports = [
   {
-    name: 'iconFont',
-    type: 'list',
-    message: 'Select icon font',
-    choices: ['a', 'c'],
-    default: 'a'
-  },
-  {
-    name: 'useTheme',
-    type: 'confirm',
-    message: 'Use custom theme?',
-    default: false
+    name: 'someFlag',
+    // 确认对话只在用户已经选取了特性的时候展示
+    when: answers => answers.features.include('my-feature'),
+    message: 'Do you want to turn on flag foo?',
+    type: 'confirm'
   }
 ]
