8d24ceadc8e05d2b0337882eeedc50c5630fd3de
施永坚 <syj_188888888@163.com>
2018-11-11 18:09:14 +0800

    添加prompts示例

--- prompts.js
+++ prompts.js
@@ -1,9 +1,8 @@
 module.exports = [
   {
-    name: 'someFlag',
+    name: '加入Vue Router',
     // 确认对话只在用户已经选取了特性的时候展示
-    when: answers => answers.features.include('my-feature'),
-    message: 'Do you want to turn on flag foo?',
+    message: '会内置Vue Router',
     type: 'confirm'
   }
 ]
