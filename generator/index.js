67581ff255302e13cddf1cda34ad297f0338c7da
施永坚 <syj_188888888@163.com>
2018-11-12 14:19:40 +0800

    添加一些依赖

--- generator/index.js
+++ generator/index.js
@@ -1,2 +1,45 @@
 module.exports = (api, options) => {
+  console.log(api, options)
+
+  if (options.common) {
+
+  }
+
+  if (options.api) {
+
+  }
+
+  if (options.components) {}
+
+  if (options.i18n) {
+    api.extendPackage({
+      dependencies: {
+        'vue-i18n': '^8.1.0'
+      }
+    })
+  }
+
+  if (options.router) {
+    api.extendPackage({
+      dependencies: {
+        'vue-router': '^3.0.1'
+      }
+    })
+  }
+
+  if (options.vuex) {
+    api.extendPackage({
+      dependencies: {
+        'vuex': '^3.0.1'
+      }
+    })
+  }
+
+  if (options.axios) {
+    api.extendPackage({
+      dependencies: {
+        'axios': '^0.18.0'
+      }
+    })
+  }
 }
