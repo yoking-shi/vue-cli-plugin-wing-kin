f87fccdb866d398ee62c93cdb60d51b9d9fd7199
施永坚 <syj_188888888@163.com>
2018-11-12 13:48:02 +0800

    添加各种选项

--- prompts.js
+++ prompts.js
@@ -1,8 +1,37 @@
 module.exports = [
   {
-    name: '加入Vue Router',
-    // 确认对话只在用户已经选取了特性的时候展示
-    message: '会内置Vue Router',
+    name: 'common',
+    message: '添加公用类库',
+    type: 'confirm'
+  },
+  {
+    name: 'components',
+    message: '添加组件文件夹',
+    type: 'confirm'
+  },
+  {
+    name: 'i18n',
+    message: '添加i18n国际化',
+    type: 'confirm'
+  },
+  {
+    name: 'router',
+    message: '添加Vue-Router',
+    type: 'confirm'
+  },
+  {
+    name: 'vuex',
+    message: '添加Vuex',
+    type: 'confirm'
+  },
+  {
+    name: 'axios',
+    message: '添加axios',
+    type: 'confirm'
+  },
+  {
+    name: 'api',
+    message: '添加API列表文件夹',
     type: 'confirm'
   }
 ]
